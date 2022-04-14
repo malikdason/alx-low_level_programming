6a99c10ae137bd4bd411971c8551660c6765d6d0
Malik Dason <79647420+malikdason@users.noreply.github.com>
2022-04-14 22:38:40 +0000

    modified I-supper file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit - checks if parameter is a number between 0 to 9.
- * @c: input number.
- * Return: 1 if is a number (0 to 9), 0 in other case.
+ * _isupper - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
  */
-int _isdigit(int c)
+
+int _isupper(int c)
 {
-	if (c >= 48 && c <= 57)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c >= 65 && c <= 90);
+
 }
